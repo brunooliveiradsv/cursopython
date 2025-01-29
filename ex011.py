8c18ffba04e97e2680eb14834180bfb2d6a28498
Bruno <brunooliveiradsv@gmail.com>
2025-01-29 12:38:06 -0300

    New

--- ex011.py
+++ ex011.py
@@ -1,12 +1,13 @@
-n = int(input('Digite um número inteiro: '))
-t1 = n * 1
-t2 = n * 2
-t3 = n * 3
-t4 = n * 4
-t5 = n * 5
-t6 = n * 6
-t7 = n * 7
-t8 = n * 8
-t9 = n * 9
-t10 = n * 10
-print(f'A tabuada de {n}:\n{n}x1={t1}\n{n}x2={t2}\n{n}x3={t3}\n{n}x4={t4}\n{n}x5={t5}\n{n}x6={t6}\n{n}x7={t7}\n{n}x8={t8}\n{n}x9={t9}\n{n}x10={t10}')
+n = int(input('Digite um número pra ver a tabuada: '))
+print('='*20)
+print(f'{n} x {1} = {n*1}')
+print(f'{n} x {2} = {n*2}')
+print(f'{n} x {3} = {n*3}')
+print(f'{n} x {4} = {n*4}')
+print(f'{n} x {5} = {n*5}')
+print(f'{n} x {6} = {n*6}')
+print(f'{n} x {7} = {n*7}')
+print(f'{n} x {8} = {n*8}')
+print(f'{n} x {9} = {n*9}')
+print(f'{n} x {10} = {n*10}')
+print('='*15)
--- ex014.py
+++ ex014.py
@@ -1,3 +1,2 @@
-salario = float(input('Digite seu salario R$'))
-aumento = salario + (salario * 15/100)
-print(f'Seu salário no momento é de R${salario} mas terá um aumento de 15% e ficará R${aumento}')
+sal = float(input('Digite seu salario R$'))
+print(f'Seu salário no momento é de R${sal} mas terá um aumento de 15% e ficará R${sal + (sal * 15/100)}')
