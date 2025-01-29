d4922140fe48d108f650be5011c5ea8f8cc0eda0
Bruno <brunooliveiradsv@gmail.com>
2025-01-29 13:19:58 -0300

    New

---
+++ Módulos/ex000.py
@@ -0,0 +1,5 @@
+from math import sqrt
+
+num = int(input('Digite um número: '))
+raiz = sqrt(num)
+print(f'A Raiz de {num} é igual a {raiz}')
--- ex012.py
+++
@@ -1,3 +0,0 @@
-wallet = float(input('Quanto você tem na carteira? R$'))
-convert = wallet * 5.87
-print(f'Com R${wallet} você poderá comprar US${convert}')
